a487705b078122d4dd9df791cfec8ec8100b1f03
anant2355 <achalumu@depaul.edu>
2023-08-23 12:36:37 -0500

    added app folder

--- app/api/process.py
+++ app/api/process.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 from flask import Flask
 from dotenv import load_dotenv
 import configargparse as argparse
@@ -11,22 +12,42 @@ load_dotenv(os.path.join(os.path.join(os.getcwd(), "biofilm.env")))
 class Process():
     def __init__(self, ):
         self.args = self.get_arguments()
+        self.config = self.get_config()
         
-    def get_arguments(self):
+    def get_arguments(self, ):
         """
             Parse environment variables.
             :return: argument parsed
         """
         try:
             parser = argparse.ArgumentParser(description='biofilm_api')
-            parser.add_argument('--host', required=False, env_var='HOST')
-            parser.add_argument('--username', required=False, env_var='USERNAME')
-            parser.add_argument('--password', required=False, env_var='PASSWORD')
+            parser.add_argument('--db_host_name', required=False, env_var='DB_HOST')
+            parser.add_argument('--db_user', required=False, env_var='DB_USER')
+            parser.add_argument('--db_paswd', required=False, env_var='DB_PWD')
+            parser.add_argument('--db_name', required=False, env_var='DB_NAME')
             parser.add_argument('--default_request_rate_limit', required=False, type=int, env_var='DEFAULT_REQUEST_RATE_LIMIT', default=10)
+            parser.add_argument('--config_file_name', required=False, env_var='CONFIG_FILE_NAME')
             return parser.parse_args([])
         
         except Exception as e:
             logger.error(f"While processing in Function {Process.get_arguments.__qualname__}, we got {sys.exc_info()[0]} Exception. \n '{e}' in Line Number {sys.exc_info()[2].tb_lineno}  File Name {os.path.basename(sys.exc_info()[2].tb_frame.f_code.co_filename)}")
+            
+    def get_config(self, ):
+        """
+            Load config variables:
+            :return: configuration data
+        """
+        try:
+            config_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), self.args.config_file_name)
+            
+            with open(config_file_path, 'r') as fp:
+                config = json.load(fp)
+
+            return config
+        
+        except Exception as e:
+            logger.error(f"While processing in Function {Process.get_config.__qualname__}, we got {sys.exc_info()[0]} Exception. \n '{e}' in Line Number {sys.exc_info()[2].tb_lineno}  File Name {os.path.basename(sys.exc_info()[2].tb_frame.f_code.co_filename)}")
+            raise
             
             
     def get_app(self, ):
--- app/api/service.py
+++ app/api/service.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import mysql.connector
+import pymysql
 from core.logger import get_logger
 from app.api.process import Process
 from flask import jsonify, make_response
@@ -11,24 +11,30 @@ app=processor.get_app()
 
 def db_conn():
     try:
-        mydb = mysql.connector.connect(
-            host=processor.args.host,
-            username=processor.args.username,
-            password=processor.args.password)
-        return mydb
+        connection = pymysql.connect(host=processor.args.db_host_name,
+                                     user=processor.args.db_user,
+                                     password=processor.args.db_paswd,
+                                     database=processor.args.db_name,
+                                     cursorclass=pymysql.cursors.DictCursor)
+            
+            
+        if(connection):
+            logger.info('Connection created successfully')
+        return connection
     except Exception as e:
             logger.error(f"While processing in Function {db_conn.__qualname__}, we got {sys.exc_info()[0]} Exception. \n '{e}' in Line Number {sys.exc_info()[2].tb_lineno}  File Name {os.path.basename(sys.exc_info()[2].tb_frame.f_code.co_filename)}")
 
 @app.route('/db', methods=['GET'])            
 def return_db():
     try:
-        mydb = db_conn()
-        cur = mydb.cursor()
-        cur.execute("show tables from biofilm;")
-        results = cur.fetchall()
-        cur.close()
+        connection = db_conn()
+        with connection.cursor() as cur:
+            cur.execute("select * from Biofilm.organisms limit 5;")
+            results = cur.fetchall()
+            cur.close()
         return results
     
     except Exception as e:
             logger.error(f"While processing in Function {return_db.__qualname__}, we got {sys.exc_info()[0]} Exception. \n '{e}' in Line Number {sys.exc_info()[2].tb_lineno}  File Name {os.path.basename(sys.exc_info()[2].tb_frame.f_code.co_filename)}")
         
+
