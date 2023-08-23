5cad74a969232c8a08ccb921bd1461f72b8adbe2
anant2355 <achalumu@depaul.edu>
2023-08-23 12:53:23 -0500

    testing initial correct clone

--- app_start.py
+++ app_start.py
@@ -2,4 +2,4 @@ import os
 from app.api.service import app
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ['API_PORT']),debug=True, host='0.0.0.0')
+    app.run(port=int(os.environ['API_PORT']),debug=True, host='0.0.0.0') 
